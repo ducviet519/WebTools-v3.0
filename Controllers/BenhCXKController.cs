a35d62688da0a0471339dff72eec4147c89b6ad0
DucViet519 <ducviet519@gmail.com>
2022-10-10 16:35:11 +0700

    ok

---
+++ Controllers/BenhCXKController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTools.Controllers
+{
+    public class BenhCXKController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> PhieuKhamAsync()
+        {
+            return View();
+        }
+    }
+}
