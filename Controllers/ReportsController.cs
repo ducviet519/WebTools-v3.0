bf846f32916556f1d2f57ab9f0b1d5c658a515e2
DucViet519 <ducviet519@gmail.com>
2022-11-29 08:47:52 +0700

    ok

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using WebTools.Extensions;
@@ -20,7 +22,10 @@ namespace WebTools.Controllers
         {
             ExportReport data = new ExportReport();
             data.BangKeVienPhi = await (_bangKeChiPhiSevices.GetBangKeChiPhi(id, loai, "1"));
-            data.ThongTinNguoiBenh = (await (_bangKeChiPhiSevices.GetBangKeChiPhi(id, loai, "1"))).Where(i => i.hoten != null).FirstOrDefault();
+            var thongtinnguoibenh = (await (_bangKeChiPhiSevices.GetBangKeChiPhi(id, loai, "1"))).Where(i => i.hoten != null).FirstOrDefault();
+            thongtinnguoibenh.ngaysinh = Convert.ToDateTime(thongtinnguoibenh.ngaysinh, CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
+            thongtinnguoibenh.ngayvao = Convert.ToDateTime(thongtinnguoibenh.ngayvao, CultureInfo.InvariantCulture).ToString("dd/MM/yyyy hh:mm");
+            data.ThongTinNguoiBenh = thongtinnguoibenh;
             data.BarCode = StaticHelper.GenBarCode(data.ThongTinNguoiBenh.mabn);
             data.TenPhieuIn = "BangKeVienPhi";
             return View(data);
